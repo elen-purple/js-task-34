e308fc3d1fa6d5edd1d06ac78f4e22868921276c
elen-purple <anna.havrylova.2011@gmail.com>
2025-04-07 19:47:38 +0300

    try 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,23 @@
 import { getCardsLayout } from "./get-cards-layout";
 import { setCardsLayout } from "./set-cards-layout";
 
-let page = 1;
+let page;
+if (Object.keys(localStorage).includes("page")) {
+  page = Number.parseInt(localStorage.getItem("page"));
+} else {
+  page = 1;
+  localStorage.setItem("page", page);
+}
 
-getCardsLayout(page).then(({ hits }) => {
-  setCardsLayout(hits);
-});
+for (let i = 0; i < page; i += 1) {
+  getCardsLayout(i + 1).then(({ hits }) => {
+    setCardsLayout(hits);
+  });
+}
 
 document.querySelector("#load-btn").addEventListener("click", () => {
   page += 1;
+  localStorage.setItem("page", page);
   getCardsLayout(page).then(({ hits }) => {
     setCardsLayout(hits);
   });
